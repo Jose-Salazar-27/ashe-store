a82a84715cfca8cb5602deab022b0140085754fc
Jose Andres Salazar <joseandres61@gmail.com>
2022-10-13 06:54:28 -0400

    feat: create dark and light themes for theme provider

---
+++ src/styled-components/app-theme.js
@@ -0,0 +1,17 @@
+export const appTheme = {
+  light: {
+    backgroundColor: '#fff',
+    secondBackgroundColor: '#F5F3F4',
+    fontColor: '#000',
+    primaryColor: '#0046BE',
+    gray: '#343434',
+  },
+
+  dark: {
+    backgroundColor: '#202124',
+    secondBackgroundColor: '#3C4043',
+    fontColor: '#E8EAED',
+    primaryColor: '#0046BE',
+    gray: '#343434',
+  },
+};
