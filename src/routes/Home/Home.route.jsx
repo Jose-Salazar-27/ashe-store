c672fb3e17aab731ee2f8a330bc198c9e833d5b1
Jose Andres Salazar <joseandres61@gmail.com>
2022-10-13 06:55:16 -0400

    feat: create home route

---
+++ src/routes/Home/Home.route.jsx
@@ -0,0 +1,9 @@
+// External libraries dependencies
+import { useState } from 'react';
+import axios from 'axios';
+
+// Components and Styled components
+
+export default function Home() {
+  return <div>Home.route</div>;
+}
