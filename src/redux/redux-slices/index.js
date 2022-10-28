0549816601e9082392180bd1878eda5db31c54c8
Jose Andres Salazar <joseandres61@gmail.com>
2022-10-27 22:10:26 -0400

    chore: create products slices

--- src/redux/redux-slices/index.js
+++ src/redux/redux-slices/index.js
@@ -1,1 +1,2 @@
-export { default as userReducer, set, unset, userSlice, test, userSelector, getProducts } from './user.slice';
+export { default as userReducer, set, unset, userSlice, test, userSelector } from './user.slice';
+export { default as productsReducer, filter, get, setProducts, productsInitialState, productsSelector, productsSlice } from './products.slice';
