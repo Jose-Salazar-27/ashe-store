d0403f50b52e1f11800fd09e9e8e40f51bcbe5ca
Jose Andres Salazar <joseandres61@gmail.com>
2022-10-27 22:04:35 -0400

    feat: create Slider component and its atoms

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { Navbar } from './Navbar/Navbar.component';
 export { CategoriesMenu } from './CategoriesMenu/CategoriesMenu.component';
 export { Carousel } from './Carousel/Carousel.component';
+export { Slider } from './Slider/Slider.component';
+export { Card } from './Card/Card.component';
